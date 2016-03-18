abaf723c026b69efd26f4a89b04470272d63fb32
nkoenig <devnull@localhost>
2016-03-18 12:48:12 -0700

    Updates

--- configure.bat
+++ configure.bat
@@ -3,7 +3,6 @@
 @echo Configuring for build type %build_type%
 
 @set PROTOBUF_PATH=%cd%\..\..\protobuf-2.6.0-win64-vc12
-@set path=%path%;c:\ruby22-x64\bin
 
 cmake -G "NMake Makefiles"^
   -DPROTOBUF_SRC_ROOT_FOLDER="%PROTOBUF_PATH%"^
