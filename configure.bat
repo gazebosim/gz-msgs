da5bc14f0ebc476f3b69b4d5695370a597737af5
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-04 13:48:05 +0200

    Include ignition-math in conf script

--- configure.bat
+++ configure.bat
@@ -3,8 +3,10 @@
 @echo Configuring for build type %build_type%
 
 @set PROTOBUF_PATH=%cd%\..\..\protobuf-2.6.0-win64-vc12
+@set IGNITION-MATH_PATH=%cd%\..\..\ign-math\build\install\%build_type%
 
 cmake -G "NMake Makefiles"^
+  -DCMAKE_PREFIX_PATH="%IGNITION-MATH_PATH%"^
   -DPROTOBUF_SRC_ROOT_FOLDER="%PROTOBUF_PATH%"^
   -DCMAKE_INSTALL_PREFIX="install\%build_type%"^
   -DCMAKE_BUILD_TYPE="%build_type%"^
