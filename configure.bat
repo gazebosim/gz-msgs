fed9f16bb5fbcbc73dfbab264de057c6c64c7928
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-04 14:19:08 +0200

    Fix all references to ign-math var

--- configure.bat
+++ configure.bat
@@ -3,10 +3,10 @@
 @echo Configuring for build type %build_type%
 
 @set PROTOBUF_PATH=%cd%\..\..\protobuf-2.6.0-win64-vc12
-@set IGNITION_MATH_PATH=%cd%\..\..\ign-math\build\install\%build_type%
+@set IGN_MATH_PATH=%cd%\..\..\ign-math\build\install\%build_type%
 
 cmake -G "NMake Makefiles"^
-  -DCMAKE_PREFIX_PATH="%IGNITION-MATH_PATH%"^
+  -DCMAKE_PREFIX_PATH="%IGN_MATH_PATH%"^
   -DCMAKE_INSTALL_PREFIX="install\%build_type%"^
   -DCMAKE_BUILD_TYPE="%build_type%"^
   -DPROTOBUF_SRC_ROOT_FOLDER="%PROTOBUF_PATH%"^
