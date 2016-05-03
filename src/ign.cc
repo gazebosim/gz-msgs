d0b4428126ba39cf2c523485c57fdd2679049ba0
Nate Koenig <natekoenig@gmail.com>
2016-05-03 13:35:16 -0700

    Include order

--- src/ign.cc
+++ src/ign.cc
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * */
-#include <iostream>
 #include <google/protobuf/descriptor.h>
+#include <iostream>
 #include "ignition/msgs/msgs.hh"
 
 #ifdef _MSC_VER
