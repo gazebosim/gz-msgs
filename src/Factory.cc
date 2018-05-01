2d4e7376ba03164f2b98a28c09a6de6cbeb311c9
Carlos Agüero <caguero@osrfoundation.org>
2018-05-01 14:24:42 -0700

    Ignore Protobuf warnings on Windows

--- src/Factory.cc
+++ src/Factory.cc
@@ -17,7 +17,7 @@
 
 #ifdef _MSC_VER
 #pragma warning(push)
-#pragma warning(disable: 4251)
+#pragma warning(disable: 4146 4251)
 #endif
 
 #include <google/protobuf/descriptor.pb.h>
--- src/Factory_TEST.cc
+++ src/Factory_TEST.cc
@@ -58,9 +58,16 @@ TEST(FactoryTest, New)
 /////////////////////////////////////////////////
 TEST(FactoryTest, NewDynamicFactory)
 {
+  auto msg = msgs::Factory::New("example.msgs.StringMsg");
+  ASSERT_TRUE(msg.get() != nullptr);
+}
+
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
   // Point IGN_DESCRIPTOR_DIR to the directory with the .desc file.
   setenv("IGN_DESCRIPTOR_DIR", PROJECT_SOURCE_PATH "/test/desc", 1);
 
-  auto msg = msgs::Factory::New("example.msgs.StringMsg");
-  ASSERT_TRUE(msg.get() != nullptr);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
