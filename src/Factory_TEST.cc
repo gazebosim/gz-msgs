ff43f935e59a4a49a21dac44d477ba12b25a67da
Michael Carroll <michael@openrobotics.org>
2020-06-02 15:19:47 -0500

    Bazel build rules for msgs
    Co-authored-by: John Shepherd <john@openrobotics.org>
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- src/Factory_TEST.cc
+++ src/Factory_TEST.cc
@@ -19,7 +19,7 @@
 #include <algorithm>
 #include <cstddef>
 
-#include "ignition/msgs/test_config.h"
+#include "test_config.h"
 #include "ignition/msgs/MessageTypes.hh"
 #include "ignition/msgs/Factory.hh"
 
