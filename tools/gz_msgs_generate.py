85a2b5d0bad365558cd69dd8d6e0f133773add36
Michael Carroll <mjcarroll@intrinsic.ai>
2023-07-12 20:33:01 +0000

    Revert "Generate messages in downstream builds (#339)"
    This reverts commit 0c78b9663ec669272209f32b4506191839c37228.
    
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- tools/gz_msgs_generate.py
+++ tools/gz_msgs_generate.py
@@ -35,7 +35,14 @@ def main(argv=sys.argv[1:]):
         help='Flag to indicate if C++ bindings should be generated',
         action='store_true')
     parser.add_argument(
+        '--generate-ruby',
+        help='Flag to indicate if Ruby bindings should be generated',
+        action='store_true')
+    parser.add_argument(
         '--output-cpp-path',
+        help='The basepath of the generated C++ files')
+    parser.add_argument(
+        '--output-ruby-path',
         help='The basepath of the generated C++ files')
     parser.add_argument(
         '--proto-path',
@@ -45,63 +52,60 @@ def main(argv=sys.argv[1:]):
     parser.add_argument(
         '--input-path',
         required=True,
-        help='The location of the template files')
-    parser.add_argument(
-        '--dependency-proto-paths',
-        nargs='*',
-        help='The location of the protos')
+        help='The location of the template files',
+        action='append')
     args = parser.parse_args(argv)
 
-    # First generate the base cpp files
-    cmd = [args.protoc_exec]
+    for input_file in args.input_path:
+        # First generate the base cpp and ruby files
+        cmd = [args.protoc_exec]
 
-    for path in args.proto_path:
-        cmd += [f'--proto_path={path}']
+        for pp in args.proto_path:
+            cmd += [f'--proto_path={pp}']
 
-    if args.dependency_proto_paths:
-        for path in args.dependency_proto_paths:
-            cmd += [f'--proto_path={path}']
+        if args.generate_cpp:
+            cmd += [f'--plugin=protoc-gen-ignmsgs={args.gz_generator_bin}']
+            cmd += [f'--cpp_out=dllexport_decl=GZ_MSGS_VISIBLE:{args.output_cpp_path}']
+            cmd += [f'--ignmsgs_out={args.output_cpp_path}']
+        if args.generate_ruby:
+            cmd += [f'--ruby_out=dllexport_decl=GZ_MSGS_VISIBLE:{args.output_ruby_path}']
+        cmd += [input_file]
 
-    if args.generate_cpp:
-        cmd += [f'--plugin=protoc-gen-ignmsgs={args.gz_generator_bin}']
-        cmd += [f'--cpp_out={args.output_cpp_path}']
-        cmd += [f'--ignmsgs_out={args.output_cpp_path}']
-    cmd += [args.input_path]
+        try:
+            subprocess.check_call(cmd)
+        except subprocess.CalledProcessError as e:
+            print(f'Failed to execute protoc compiler: {e}')
+            sys.exit(-1)
 
-    os.makedirs(args.output_cpp_path, exist_ok=True)
+        # Move original generated cpp to details/
+        proto_file = os.path.splitext(os.path.relpath(input_file, args.proto_path[0]))[0]
+        detail_proto_file = proto_file.split(os.sep)
 
-    try:
-        subprocess.check_call(cmd)
-    except subprocess.CalledProcessError as e:
-        print(f'Failed to execute protoc compiler: {e}')
-        sys.exit(-1)
+        detail_proto_dir = detail_proto_file[:-1]
+        detail_proto_dir.append('details')
+        detail_proto_dir = os.path.join(*detail_proto_dir)
+        detail_proto_file.insert(-1, 'details')
+        detail_proto_file = os.path.join(*detail_proto_file)
 
-    # Move original generated cpp to details/
-    proto_file = os.path.splitext(os.path.relpath(args.input_path, args.proto_path[0]))[0]
-    detail_proto_file = proto_file.split(os.sep)
+        header = os.path.join(args.output_cpp_path, proto_file + ".pb.h")
+        gz_header = os.path.join(args.output_cpp_path, proto_file + ".gz.h")
+        detail_header = os.path.join(args.output_cpp_path, detail_proto_file + ".pb.h")
 
-    detail_proto_dir = detail_proto_file[:-1]
-    detail_proto_dir.append('details')
-    detail_proto_dir = os.path.join(*detail_proto_dir)
-    detail_proto_file.insert(-1, 'details')
-    detail_proto_file = os.path.join(*detail_proto_file)
+        if proto_file.find('google/protobuf') >= 0:
+            continue
 
-    header = os.path.join(args.output_cpp_path, proto_file + ".pb.h")
-    gz_header = os.path.join(args.output_cpp_path, proto_file + ".gz.h")
-    detail_header = os.path.join(args.output_cpp_path, detail_proto_file + ".pb.h")
+        try:
+            os.makedirs(os.path.join(args.output_cpp_path, detail_proto_dir),
+                    exist_ok=True)
+            # Windows cannot rename a file to an existing file
+            if os.path.exists(detail_header):
+                os.remove(detail_header)
 
-    try:
-        os.makedirs(os.path.join(args.output_cpp_path, detail_proto_dir),
-                exist_ok=True)
-        # Windows cannot rename a file to an existing file
-        if os.path.exists(detail_header):
-            os.remove(detail_header)
-
-        os.rename(header, detail_header)
-        os.rename(gz_header, header)
-    except Exception as e:
-        print(f'Failed to manipulate gz-msgs headers: {e}')
-        sys.exit(-1)
+            os.rename(header, detail_header)
+            os.rename(gz_header, header)
+        except Exception as e:
+            print(f'Failed to manipulate gz-msgs headers: {e}')
+            sys.exit(-1)
 
 if __name__ == '__main__':
     sys.exit(main())
