408518b450a08c7acd45555d040bfa9d95b2a5aa
Nate Koenig <natekoenig@gmail.com>
2023-07-27 05:50:27 -0700

    Remove ignition
    Signed-off-by: Nate Koenig <natekoenig@gmail.com>

--- include/gz/msgs/Factory.hh
+++ include/gz/msgs/Factory.hh
@@ -64,16 +64,8 @@ namespace gz
       public: template<typename T>
               static std::unique_ptr<T> New(const std::string &_msgType)
               {
-                auto msgType = _msgType;
-                if (msgType.find("ignition") == 0)
-                {
-                  msgType.replace(0, 8, "gz");
-                  std::cerr << "Trying to create deprecated message type ["
-                            << _msgType << "]. Using [" << msgType
-                            << "] instead." << std::endl;
-                }
-                return std::unique_ptr<T>(
-                    static_cast<T*>(New(msgType).release()));
+               return std::unique_ptr<T>(
+                    static_cast<T*>(New(_msgType).release()));
               }
 
       /// \brief Create a new instance of a message.
@@ -85,16 +77,8 @@ namespace gz
               static std::unique_ptr<T> New(const std::string &_msgType,
                   const std::string &_args)
               {
-                auto msgType = _msgType;
-                if (msgType.find("ignition") == 0)
-                {
-                  msgType.replace(0, 8, "gz");
-                  std::cerr << "Trying to create deprecated message type ["
-                            << _msgType << "]. Using [" << msgType
-                            << "] instead." << std::endl;
-                }
                 return std::unique_ptr<T>(
-                    static_cast<T*>(New(msgType, _args).release()));
+                    static_cast<T*>(New(_msgType, _args).release()));
               }
 
       /// \brief Create a new instance of a message.
