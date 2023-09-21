3550a643791987a7b21a48c31dcd56ae8b4f628e
Michael Carroll <mjcarroll@intrinsic.ai>
2023-09-21 16:26:37 +0000

    Return nullptr when message fails to populate
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- core/src/MessageFactory.cc
+++ core/src/MessageFactory.cc
@@ -115,7 +115,12 @@ MessageFactory::MessagePtr MessageFactory::New(
   std::unique_ptr<google::protobuf::Message> msg = New(_msgType);
   if (msg)
   {
-    google::protobuf::TextFormat::ParseFromString(_args, msg.get());
+    if (!google::protobuf::TextFormat::ParseFromString(_args, msg.get()))
+    {
+      // The user-provided string was invalid,
+      // return nullptr rather than an empty message.
+      msg.reset();
+    }
   }
   return msg;
 }
