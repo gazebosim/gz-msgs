0622409f7e44e156af7b8e3842cc0521ae304cca
Silvio Traversaro <silvio.traversaro@iit.it>
2023-10-11 15:51:57 +0200

    Fix cross-compilation support for gz-msg10 (#392)
    Signed-off-by: Silvio Traversaro <silvio.traversaro@iit.it>
    Co-authored-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- cmake/gz_msgs_protoc.cmake
+++ cmake/gz_msgs_protoc.cmake
@@ -4,6 +4,7 @@
 #   GENERATE_CPP        - generates c++ code for the message if specified
 #   GENERATE_PYTHON     - generates python code for the message if specified
 # One value arguments:
+#   PYTHON_INTERPRETER  - Target or path to the Python interpreter to use to run generation scripts
 #   MSGS_GEN_SCRIPT     - Path to the message generation python script
 #   PROTO_PACKAGE       - Protobuf package the file belongs to (e.g. "gz.msgs")
 #   PROTOC_EXEC         - Path to protoc
@@ -22,6 +23,7 @@
 function(gz_msgs_protoc)
   set(options GENERATE_CPP GENERATE_PYTHON)
   set(oneValueArgs
+    PYTHON_INTERPRETER
     MSGS_GEN_SCRIPT
     PROTO_PACKAGE
     PROTOC_EXEC
@@ -37,6 +39,13 @@ function(gz_msgs_protoc)
   set(multiValueArgs PROTO_PATH DEPENDENCY_PROTO_DESCS)
 
   cmake_parse_arguments(gz_msgs_protoc "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  if (NOT DEFINED gz_msgs_protoc_PYTHON_INTERPRETER)
+    set(gz_msgs_protoc_PYTHON_INTERPRETER Python3::Interpreter)
+  endif()
+  if (NOT DEFINED gz_msgs_protoc_PROTOC_EXEC)
+    set(gz_msgs_protoc_PROTOC_EXEC protobuf::protoc)
+  endif()
 
   get_filename_component(ABS_FIL ${gz_msgs_protoc_INPUT_PROTO} ABSOLUTE)
   get_filename_component(FIL_WE ${gz_msgs_protoc_INPUT_PROTO} NAME_WE)
@@ -85,8 +94,14 @@ function(gz_msgs_protoc)
     set(${gz_msgs_protoc_OUTPUT_PYTHON_VAR} ${${gz_msgs_protoc_OUTPUT_PYTHON_VAR}} PARENT_SCOPE)
   endif()
 
+  if(TARGET ${gz_msgs_protoc_PROTOC_EXEC})
+    set(gz_msgs_protoc_PROTOC_EXEC_FILE_ABS_PATH "$<TARGET_FILE:${gz_msgs_protoc_PROTOC_EXEC}>")
+  else()
+    set(gz_msgs_protoc_PROTOC_EXEC_FILE_ABS_PATH ${gz_msgs_protoc_PROTOC_EXEC})
+  endif()
+
   set(GENERATE_ARGS
-      --protoc-exec "$<TARGET_FILE:${gz_msgs_protoc_PROTOC_EXEC}>"
+      --protoc-exec "${gz_msgs_protoc_PROTOC_EXEC_FILE_ABS_PATH}"
       --gz-generator-bin "${gz_msgs_protoc_GZ_PROTOC_PLUGIN}"
       --proto-path "${gz_msgs_protoc_PROTO_PATH}"
       --input-path "${ABS_FIL}"
@@ -118,7 +133,7 @@ function(gz_msgs_protoc)
 
   add_custom_command(
     OUTPUT ${output_files}
-    COMMAND Python3::Interpreter
+    COMMAND ${gz_msgs_protoc_PYTHON_INTERPRETER}
     ARGS ${gz_msgs_protoc_MSGS_GEN_SCRIPT} ${GENERATE_ARGS}
     DEPENDS
       ${ABS_FIL}
