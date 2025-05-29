8349c8df18599a27b6c2dcb57bbceb24b8a4c857
Steve Peters <scpeters@openrobotics.org>
2025-05-29 10:30:18 -0700

    Fix gz msgs ruby command (#516)
    Fixes #462.
    Test gz msg command in CI.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    (cherry picked from commit e3ad7dbb2c5d711b7deb74e7a51710c206f5caa1)

--- .github/ci/after_make.sh
+++ .github/ci/after_make.sh
@@ -1,2 +1,6 @@
 # It's necessary to install the python modules for the test.
 make install
+# Test gz command
+export GZ_CONFIG_PATH=/usr/local/share/gz
+export LD_LIBRARY_PATH=/usr/local/lib
+gz msg
