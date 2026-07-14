d1480d37bc2bc5ff0e5180a15c406b56f0ca2d5d
Shameek Ganguly <shameek@intrinsic.ai>
2026-07-14 09:53:00 -0700

    Add back detail::dynamic_message_cast function, and deprecate it
    Signed-off-by: Shameek Ganguly <shameek@intrinsic.ai>

---
+++ core/include/gz/msgs/detail/dynamic_message_cast.hh
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2023 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef GZ_MSGS_DETAIL_DYNAMIC_POINTER_CAST_HH_
+#define GZ_MSGS_DETAIL_DYNAMIC_POINTER_CAST_HH_
+
+#include <memory>
+#include <utility>
+
+#include <google/protobuf/message.h>
+
+#include "gz/msgs/MessageCastUtils.hh"
+
+namespace gz::msgs::detail
+{
+
+/// Cast a base unique pointer to protobuf message type to child type
+/// Deprecated: Use DoDynamicCast instead.
+template<typename MsgT>
+std::unique_ptr<MsgT> GZ_DEPRECATED(12)
+dynamic_message_cast(std::unique_ptr<google::protobuf::Message> &&_baseMsg)
+{
+ return DoDynamicCastMessage<MsgT>(std::move(_baseMsg));
+}
+
+}  // namespace gz::msgs::detail
+
+#endif  // GZ_MSGS_DETAIL_DYNAMIC_POINTER_CAST_HH_
