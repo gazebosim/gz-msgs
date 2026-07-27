783107a9a3cf126dc810b605544ca7eb52fbb2ad
Shameek Ganguly <shameek@intrinsic.ai>
2026-07-27 15:12:09 -0700

    Add back detail::dynamic_message_cast function
    Signed-off-by: Shameek Ganguly <shameek@intrinsic.ai>

--- core/include/gz/msgs/detail/dynamic_message_cast.hh
+++ core/include/gz/msgs/detail/dynamic_message_cast.hh
@@ -29,6 +29,8 @@
 #pragma warning(pop)
 #endif
 
+#include "gz/msgs/MessageCastUtils.hh"
+
 namespace gz::msgs::detail
 {
 
@@ -37,17 +39,7 @@ template<typename MsgT>
 std::unique_ptr<MsgT>
 dynamic_message_cast(std::unique_ptr<google::protobuf::Message> &&_baseMsg)
 {
-#if GOOGLE_PROTOBUF_VERSION >= 5028000
-  auto converted = std::unique_ptr<MsgT>{
-      google::protobuf::DynamicCastMessage<MsgT>(_baseMsg.get())};
-#else
-  auto converted = std::unique_ptr<MsgT>{dynamic_cast<MsgT*>(_baseMsg.get())};
-#endif
-  if (converted) {
-    // transfer ownership to a new unique_ptr object by releasing from old one
-    (void) _baseMsg.release();
-  }
-  return converted;
+  return DoDynamicCastMessage<MsgT>(std::move(_baseMsg));
 }
 
 }  // namespace gz::msgs::detail
